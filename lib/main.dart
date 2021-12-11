02304e9266a071482e04865bf6c8f93fb30c938e
wilmarv <wilmarfonseca@gec.inatel.br>
2021-12-11 11:03:23 -0300

    Configuração do ambiente e projeto iniciado

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:calculadora_idade/pageMain.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculadora de Idade',
+
+      home: PageMain(),
+    );
+  }
+}
+
---
+++ lib/pageMain.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PageMain extends StatefulWidget {
+  const PageMain({Key? key}) : super(key: key);
+
+  @override
+  _PageMainState createState() => _PageMainState();
+}
+
+class _PageMainState extends State<PageMain> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
